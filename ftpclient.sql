20ecf2681ede8a3ae5849afcc4182ec52d9f6c33
robertjdev <r.j.r.eol@gmail.com>
2017-02-16 17:12:29 -0400

    DB Backup

---
+++ ftpclient.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Community v12.3.1 (64 bit)
+MySQL - 10.1.16-MariaDB : Database - ftpclient
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ftpclient` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ftpclient`;
+
+/*Table structure for table `files` */
+
+DROP TABLE IF EXISTS `files`;
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_file` varchar(256) NOT NULL,
+  `mime` varchar(56) DEFAULT NULL,
+  `extension` varchar(256) DEFAULT NULL,
+  `uri` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Data for the table `files` */
+
+insert  into `files`(`id`,`nombre_file`,`mime`,`extension`,`uri`) values 
+(1,'','','',''),
+(4,'hahaha','','',''),
+(5,'Agenda','image/png',NULL,NULL),
+(6,'Agenda','image/png',NULL,NULL),
+(7,'Agenda.PNG','image/png','PNG',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
